10d2244afc4ff594ab3de887ce5e94547c991c2b
dooli1971039 <dooli9819@gmail.com>
2022-03-04 05:31:57 +0900

    관련 사이트 주석

--- speech/test1/sound.js
+++ speech/test1/sound.js
@@ -1,9 +1,13 @@
+//https://usecode.pw/%EC%9D%8C%EC%84%B1%EC%9D%B8%EC%8B%9D-api-web-speech-api-%EB%82%98%EB%A7%8C%EC%9D%98-%EC%8B%9C%EB%A6%AC%EB%A5%BC-%EB%A7%8C%EB%93%A4-%EC%88%98-%EC%9E%88%EC%9D%84%EA%B9%8C/
+//https://mizzo-dev.tistory.com/entry/SpeechRecognition-WebChrome-%EC%9D%8C%EC%84%B1-%EC%9D%B8%EC%8B%9D
+//https://developer.mozilla.org/en-US/docs/Web/API/Web_Speech_API
+
 window.SpeechRecognition =
     window.SpeechRecognition || window.webkitSpeechRecognition;
 
 const recognition = new SpeechRecognition();
-recognition.interimResults = true;
-recognition.lang = "ko-KR";
+recognition.interimResults = true; //아직 끝나지 않은 상태의 음성을 받을 것인지 아닌지(default: false)
+recognition.lang = "ko-KR"; //한국어 인식
 
 let h1 = document.createElement("h1");
 const words = document.querySelector(".words");
