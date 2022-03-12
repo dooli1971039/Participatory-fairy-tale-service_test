56786296855dc3ebf4f07a2cdb737f2c531cba43
dooli1971039 <dooli9819@gmail.com>
2022-03-13 04:41:23 +0900

    openpose 사용?

--- motion/test10_django2_webcam/detectme/views.py
+++ motion/test10_django2_webcam/detectme/views.py
@@ -3,44 +3,99 @@ from django.shortcuts import render
 from django.views.decorators import gzip
 from django.http import StreamingHttpResponse
 import cv2
-import threading
+
+from pathlib import Path
+
+# MPII에서 각 파트 번호, 선으로 연결될 POSE_PAIRS
+BODY_PARTS = { "Head": 0, "Neck": 1, "RShoulder": 2, "RElbow": 3, "RWrist": 4,
+                "LShoulder": 5, "LElbow": 6, "LWrist": 7, "RHip": 8, "RKnee": 9,
+                "RAnkle": 10, "LHip": 11, "LKnee": 12, "LAnkle": 13, "Chest": 14,
+                "Background": 15 }
+
+POSE_PAIRS = [ ["Head", "Neck"], ["Neck", "RShoulder"], ["RShoulder", "RElbow"],
+                ["RElbow", "RWrist"], ["Neck", "LShoulder"], ["LShoulder", "LElbow"],
+                ["LElbow", "LWrist"], ["Neck", "Chest"], ["Chest", "RHip"], ["RHip", "RKnee"],
+                ["RKnee", "RAnkle"], ["Chest", "LHip"], ["LHip", "LKnee"], ["LKnee", "LAnkle"] ]
+
+# 각 파일 path
+BASE_DIR = Path(__file__).resolve().parent
+protoFile = str(BASE_DIR)+"/file/pose_deploy_linevec_faster_4_stages.prototxt"
+weightsFile = str(BASE_DIR)+"/file/pose_iter_160000.caffemodel"
+
+# 위의 path에 있는 network 모델 불러오기
+net = cv2.dnn.readNetFromCaffe(protoFile, weightsFile)
 
 # Create your views here.
 def home(request):
     return render(request,"home.html") #home.html을 호출해서 띄워준다.
 
 
-class VideoCamera(object):
-    def __init__(self):
-        self.video = cv2.VideoCapture(0)
-        (self.grabbed, self.frame) = self.video.read()
-        threading.Thread(target=self.update, args=()).start()
+def gen():
+    capture = cv2.VideoCapture(0) #카메라 정보 받아옴
+    inputWidth=320;
+    inputHeight=240;
+    inputScale=1.0/255;
+    
+    while cv2.waitKey(1) <0:
+        _, frame = capture.read()
+        frameWidth = frame.shape[1]
+        frameHeight = frame.shape[0]
+    
+        inpBlob = cv2.dnn.blobFromImage(frame, inputScale, (inputWidth, inputHeight), (0, 0, 0), swapRB=False, crop=False)
+    
+        imgb=cv2.dnn.imagesFromBlob(inpBlob)
+        
+        # network에 넣어주기
+        net.setInput(inpBlob)
 
-    def __del__(self):
-        self.video.release()
+        # 결과 받아오기
+        output = net.forward()
+        
+        # 키포인트 검출시 이미지에 그려줌
+        points = []
+        for i in range(0,15):
+            # 해당 신체부위 신뢰도 얻음.
+            probMap = output[0, i, :, :]
 
-    def get_frame(self):
-        image = self.frame
-        _, jpeg = cv2.imencode('.jpg', image)
-        return jpeg.tobytes()
+            # global 최대값 찾기
+            minVal, prob, minLoc, point = cv2.minMaxLoc(probMap)
 
-    def update(self):
-        while True:
-            (self.grabbed, self.frame) = self.video.read()
+            # 원래 이미지에 맞게 점 위치 변경
+            x = (frameWidth * point[0]) / output.shape[3]
+            y = (frameHeight * point[1]) / output.shape[2]
+
+            # 키포인트 검출한 결과가 0.1보다 크면(검출한곳이 위 BODY_PARTS랑 맞는 부위면) points에 추가, 검출했는데 부위가 없으면 None으로    
+            if prob > 0.1 :    
+                cv2.circle(frame, (int(x), int(y)), 3, (0, 255, 255), thickness=-1, lineType=cv2.FILLED) # circle(그릴곳, 원의 중심, 반지름, 색)
+                cv2.putText(frame, "{}".format(i), (int(x), int(y)), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1, lineType=cv2.LINE_AA)
+                points.append((int(x), int(y)))
+            else :
+                points.append(None)
 
 
-def gen(camera):
-    while True:
-        frame = camera.get_frame()
+
+        # 각 POSE_PAIRS별로 선 그어줌 (머리 - 목, 목 - 왼쪽어깨, ...)
+        for pair in POSE_PAIRS:
+            partA = pair[0]             # Head
+            partA = BODY_PARTS[partA]   # 0
+            partB = pair[1]             # Neck
+            partB = BODY_PARTS[partB]   # 1
+
+            #print(partA," 와 ", partB, " 연결\n")
+            if points[partA] and points[partB]:
+                cv2.line(frame, points[partA], points[partB], (0, 255, 0), 2)
+        
+        cv2.imshow("Output-Keypoints",frame)
+        
+        _,buffer=cv2.imencode(".jpg",frame)
+        frame=buffer.tobytes()
         yield(b'--frame\r\n'
-              b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
-
-
+            b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+        
 @gzip.gzip_page
 def detectme(request):
     try:
-        cam = VideoCamera() #웹캠 호출
-        return StreamingHttpResponse(gen(cam), content_type="multipart/x-mixed-replace;boundary=frame")
+        return StreamingHttpResponse(gen(), content_type="multipart/x-mixed-replace;boundary=frame")
     except:  # This is bad! replace it with proper handling
         print("에러입니다...")
         pass
