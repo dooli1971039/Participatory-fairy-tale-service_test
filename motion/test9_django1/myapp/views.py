c3e5baeac737b495f8d42edab2c1e04b39fc369a
dooli1971039 <dooli9819@gmail.com>
2022-03-13 01:34:35 +0900

    [django] update 기능 구현

--- motion/test9_django1/myapp/views.py
+++ motion/test9_django1/myapp/views.py
@@ -22,6 +22,7 @@ def HTMLTemplate(articleTag,id=None):  #id값이 없는 경우 None이 기본값
                     <input type="submit" value="delete">
                 </form>
             </li>
+            <li><a href="/update/{id}/">update</a></li>
         '''
     
     ol=''
@@ -91,3 +92,31 @@ def delete(request):
                 newTopics.append(topic)
         topics=newTopics
         return redirect('/') 
+    
+@csrf_exempt
+def update(request,id):
+    global topics
+    if request.method=='GET':
+        for topic in topics:
+            if topic["id"]==int(id):
+                selectedTopic={
+                    "title":topic["title"],
+                    "body":topic["body"]
+                }
+        article=f'''
+            <form action="/update/{id}/" method="post">
+                <p><input type="text" name="title" placeholder="title" value={selectedTopic["title"]}></p>
+                <p><textarea name="body" placeholder="body">{selectedTopic["body"]}</textarea></p>
+                <p><input type="submit"></p>
+            </form>
+        '''
+        return HttpResponse(HTMLTemplate(article,id))
+    
+    elif request.method=='POST':
+        title=request.POST['title']
+        body=request.POST['body']
+        for topic in topics:
+                if topic["id"]==int(id):
+                    topic["title"]=title
+                    topic["body"]=body
+        return redirect(f'/read/{id}')
