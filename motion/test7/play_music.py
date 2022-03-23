d61950f0b7efc8c99a99a222d8f63aa87c690784
dooli1971039 <dooli9819@gmail.com>
2022-03-24 01:39:44 +0900

    음악 재생 테스트

---
+++ motion/test7/play_music.py
@@ -0,0 +1,10 @@
+import playsound
+playsound.playsound("motion/test7/audio/motion.mp3")
+
+# import pygame
+# pygame.mixer.init()
+# pygame.mixer.music.load('motion/test7/audio/motion.mp3')
+# pygame.mixer.music.play()
+
+# import webbrowser
+# webbrowser.open("motion/test7/audio/motion.mp3")
